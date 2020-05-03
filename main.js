22f8896b93f9db7133d665c5859d07e0e1650220
Irfan Ezani <irfanezani@gmail.com>
2020-05-03 22:47:14 +0800

    refix js function

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 
 
 function convert() {
-    let numberInput = document.getElementById('input').value;
+    const numberInput = document.getElementById('input').value;
     //.value = access value.
 
-    if (numberInput === '') {
-      return alert('Please enter binary number only');
-    }
+    if (numberInput === '') return alert('Please enter binary number only');
 
     numberInput.split('').map((char) => {
-      if (char !== 1 && char !== 0) {
+      if (char !== '1' && char !== '0') {
         return alert('Please enter binary number only');
       }
     })
 
-    const decimal = numberInput.toString(2)
-    document.getElementById('answer').value = decimal
+    const decimal = parseInt(numberInput, 2);
+    document.getElementById('answer').value = decimal;
+    return true;
   }
